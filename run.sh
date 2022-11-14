d0cff279482a7209e16f7d6fbfb14e04ecc97158
Spartan0nix <l.humblot74@gmail.com>
2022-11-14 21:26:25 +0100

    [feat]: Update test operations to support coverage HTML render

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-case $1 in
+arg_1=$1
+arg_2=$2
+
+case $arg_1 in
 --test)
     podman system renumber
 
@@ -17,8 +20,17 @@ case $1 in
         i=$((i+1))
     done
 
-    echo "Running test..."
-    go test -v
+    if [[ $arg_2 == "--coverage"* ]]
+    then
+        echo "Running test... (with coverage)"
+        go test -v -coverprofile=coverage.out
+
+        echo "Formatting coverage report to HTML..."
+        go tool cover -html=coverage.out -o=coverage.html
+    else
+        echo "Running test..."
+        go test -v
+    fi
 
     echo "Destroying container stack"
     podman-compose -f ./build/container-compose-test.yml down
@@ -30,7 +42,15 @@ case $1 in
 --down)
     podman-compose -f ./build/container-compose.yml down
 ;;
-*)
+--run)
     go run  $(go list ./... | grep -v _test)
+;;
+*)
+    echo "Unknown option(s) : $*"
+    echo "Available options are :"
+    echo "--test [--coverage]"
+    echo "--up"
+    echo "--down"
+    echo "--run"
 ;;
 esac
